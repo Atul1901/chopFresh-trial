d838e428123204d1602aa941141c27031aa072be
Atul1901 <kumar.atul.20299@gmail.com>
2024-04-26 12:55:00 +0530

    completed with the footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,48 +1,50 @@
 import React from "react";
 import "./Footer.css";
+import footerLogo from "../utils/photos/footerlogo.svg";
 
 const Footer = () => {
   return (
     <>
       <div className="footer-container">
-        <div>
+        <div className="footer-head">
+          <img src={footerLogo} alt="the logo here present in footer" />
           <p>PLATEFORM</p>
         </div>
         <div>
-          <div>
+          <div className="footer-content-head">
             <p>Features</p>
           </div>
-          <div>
+          <div className="fooer-content">
             <p>Management</p>
             <p>Communication</p>
             <p>Analytic</p>
           </div>
         </div>
         <div>
-          <div>
+          <div className="footer-content-head">
             <p>Success stories</p>
           </div>
-          <div>
+          <div className="fooer-content">
             <p>Avoburger</p>
             <p>StarCoffee</p>
             <p>Forest Hotdogs</p>
           </div>
         </div>
         <div>
-          <div>
+          <div className="footer-content-head">
             <p>Social Media</p>
           </div>
-          <div>
+          <div className="fooer-content">
             <p>Instagram</p>
             <p>Facebook</p>
             <p>Youtube</p>
           </div>
         </div>
-        <div>
-          <div>
+        <div className="contact-container">
+          <div className="footer-content-head">
             <p>contact</p>
           </div>
-          <div>
+          <div className="fooer-content">
             <p>123 Main Street, Cityville, Stateville, ZIP Code</p>
             <p>hello@plateform.io</p>
           </div>
